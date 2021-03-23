50f7c76c91563da63f241edbe092e0753fc2b34e
Aakash <46834569+savantaakash@users.noreply.github.com>
2021-03-23 21:38:22 +0530

    while

---
+++ while/empwage.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -x
+
+
+#  constants
+
+IS_FTIME = 1
+IS_PTIME = 2
+
+EMP_phr = 20 
+MAX_HRS_MNTH = 50
+NUM_working_DAYS = 20
+
+
+# variables
+
+totalEMPHrs = 0
+totalWdays  = 0
+
+
+
+while [[
